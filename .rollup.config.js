30cfb734b83aa7726760da4157f844d0a689fb2c
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2016-10-08 17:14:53 +0200

    Fix build

--- .rollup.config.js
+++ .rollup.config.js
@@ -2,7 +2,7 @@ import babel from 'rollup-plugin-babel'
 import uglify from 'rollup-plugin-uglify'
 
 const isRelease = process.env[ 'BUILD_ENV' ] === 'release'
-const plugins = [ babel() ]
+const plugins = [ babel({ presets: [ 'es2015-rollup' ] }) ]
 
 if (isRelease) {
   plugins.push(uglify())
